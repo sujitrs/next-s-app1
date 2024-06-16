e8296dda72a0fee2e659bec8a47b8a8d85f20609
sujeet <suryawanshisujeet@gmail.com>
2024-06-16 22:07:52 +0530

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import styles from '@/app/ui/home.module.css';
 
 export default function Page() {
   return (
@@ -10,12 +11,13 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+        <div className={styles.shape}/>
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong>Welcome to Acmesss.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+              Next.js Learn Course !!
             </a>
-            , brought to you by Vercel.
+            , brought to you by Vercel.....
           </p>
           <Link
             href="/login"
